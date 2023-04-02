c3e300f52707c5fb8c3ebf56c98798d4de94e5df
afazad <abulfajle405@gmail.com>
2023-04-02 23:44:54 +0530

    initial Commit

---
+++ Datatypes.js
@@ -0,0 +1,46 @@
+/* Data types in Java script
+1. Non primitive data types
+*/
+// 1 : Number
+let number = 545;
+
+// 2: String
+
+let str = "Rosaline";
+
+console.log(str);
+
+
+// 3 : Undefined.
+
+let a;
+
+// 4. Boolean.
+let bool = true;
+
+// 5. Null
+
+let b = null;
+
+//6. BigInt 
+
+var bignum = 123456789n;
+
+console.log(typeof[bignum]);
+
+// 7. Symbol
+
+let  sym = "New logo";
+
+console.log(sym);
+
+// objects are key value pair
+
+let cars = {
+    car1 : "Lamboghini",
+    car2 : "Tesla",
+    car3 : "Audi",
+    car4 : "Mercedes"
+};
+
+console.log(cars);
